c1f83ce5e54fe69a78f21edd2b625ee4f54b7317
Trung DT <thanhtrung.it25@gmail.com>
2025-11-04 21:23:32 +0700

    Add and apply storybook

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { Input } from './Input';
+import { AppIcon } from '../AppIcon/AppIcon';
+import SearchIcon from "../../assets/icons/Search.svg?react";
+
+const meta = {
+  title: 'Shared/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { value: "", placeholder: "Enter text..." },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "Default"
+  },
+};
+
+export const WithValue: Story = {
+  args: {
+    value: "Some value"
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    value: "123"
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    rounded: true,
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    Icon: <AppIcon Icon={SearchIcon} size={20} />
+  },
+};
